45ae53d23ffa072fa8ae1b98b157018b3b86b45f
kfm <jwl.kungfumastah@gmail.com>
2017-01-20 14:36:01 -0600

    Added letter count section

--- name-facts.py
+++ name-facts.py
@@ -8,16 +8,21 @@ A simple app that I am developing to work on Python skills.
 ~JwL 2017
 '''
 
-## Define variables
-
+## Declare variables
 first_name = ""
 last_name = ""
+total_letters = None
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 
 ## create functions
 
 def next_section():
     input("\n\tPress the enter key to continue\n")
+
+def parse_letters():
+    d = {}
+    x = str(total_letters)
 
 
 ###########################
@@ -47,3 +52,11 @@ next_section()
 
 ## Occurences of each letter in name
 
+letter_count = first_name.lower() + last_name.lower()
+
+print("{}, here is the number of times each letter is used in your name:\n\n".format(first_name))
+
+for key in alphabet:
+	if letter_count.count(key) != 0:
+		print(key, letter_count.count(key))
+next_section()
